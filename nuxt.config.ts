242d13b0b675c3834c7d77db5f03810c6d22cbe6
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2024-08-27 22:20:39 +0400

    chore: add scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  devServer: {
+    port: 8000,
+  },
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  css: ["~/assets/scss/main.scss"],
   postcss: {
     plugins: {
       tailwindcss: {},
