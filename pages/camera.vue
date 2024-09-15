8274b6e6d6d95f3c7b62eb397416c363ead885e1
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2024-09-15 16:21:26 +0400

    feat: extract text from photo taken

--- pages/camera.vue
+++ pages/camera.vue
@@ -21,9 +21,19 @@ export default {
     };
   },
   methods: {
-    photoTaken(data) {
-      console.log("image blob: ", data.blob);
-      console.log("image data url", data.image_data_url);
+    async photoTaken(data) {
+      const fd = new FormData();
+      if (data.blob && data.image_data_url) {
+        fd.append("index", data.image_data_url);
+        fd.append("isPhotoTaken", "true");
+        fd.append("model", "prebuilt-layout"); // prebuilt-layout, MruBoatLicenseModel
+      }
+
+      const response = await $fetch("/api/extract-text-from-document", {
+        method: "POST",
+        body: fd,
+      });
+      console.log(response);
     },
   },
 };
