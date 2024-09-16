364fedc14f8f85d7d8930fadb3cc80e68e97b75b
ashfaaq2103 <ash.hobass21@gmail.com>
2024-09-16 22:53:51 +0400

    Migration of Map page from vanilla js to vue :)

---
+++ components/BoatTracker.vue
@@ -0,0 +1,457 @@
+<template>
+  <div>
+    <div id="controls">
+      <label for="startTime">Start Time:</label>
+      <input
+        type="datetime-local"
+        id="startTime"
+        v-model="startTime"
+        required
+      />
+      <label for="endTime">End Time:</label>
+      <input type="datetime-local" id="endTime" v-model="endTime" required />
+      <Button @click="submitTimes">Submit Times</Button>
+      <Button id="sendData" style="display: none" @click="sendMarkerData">
+        Send Data
+      </Button>
+    </div>
+    <client-only>
+      <div id="mapContainer" style="height: 500px; width: 100%"></div>
+    </client-only>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from "vue";
+import { useFetch } from "#app";
+import "leaflet/dist/leaflet.css";
+
+const map = ref(null);
+const L = ref(null);
+const mapInitialized = ref(false);
+const currentMarker = ref(null);
+const ellipses = ref([]);
+const ellipseMarkersCount = ref({});
+const startTime = ref("");
+const endTime = ref("");
+const userEmail = ref("john.doe@example.com"); // Placeholder email
+
+onMounted(async () => {
+  if (process.client) {
+    await loadLeaflet();
+    initializeMap();
+    await fetchAllMarkersAndUserBookings();
+  }
+});
+
+const loadLeaflet = async () => {
+  if (process.client && !L.value) {
+    const leaflet = await import("leaflet");
+    await import("leaflet/dist/leaflet.css");
+    await import("leaflet-ellipse");
+    L.value = leaflet.default || leaflet;
+  }
+};
+
+const initializeMap = async () => {
+  if (process.client && L.value) {
+    map.value = L.value.map("mapContainer", {
+      center: [-20.348404, 57.552152],
+      zoom: 10,
+      minZoom: 10,
+      maxZoom: 16,
+    });
+
+    L.value
+      .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+      .addTo(map.value);
+    ellipses.value = createEllipses();
+    setMapBounds();
+
+    const currentIcon = updateIconSize(map.value.getZoom());
+
+    map.value.on("click", (e) => handleMapClick(e, currentIcon));
+    map.value.on("zoomend", () => updateMarkerIconSize(map.value.getZoom()));
+
+    mapInitialized.value = true;
+  }
+};
+
+const setMapBounds = () => {
+  const bounds = L.value.latLngBounds(
+    L.value.latLng(-20.8, 57.2),
+    L.value.latLng(-19.8, 58.0)
+  );
+  map.value.setMaxBounds(bounds);
+  map.value.on("drag", () => {
+    map.value.panInsideBounds(bounds, { animate: false });
+  });
+};
+
+const handleMapClick = (e, icon) => {
+  const latlng = e.latlng;
+  const insideEllipse = ellipses.value.some((ellipse) =>
+    ellipse.getBounds().contains(latlng)
+  );
+
+  if (insideEllipse) {
+    if (currentMarker.value) {
+      alert(
+        "You already have a marker. Please remove it before adding a new one."
+      );
+    } else {
+      currentMarker.value = L.value.marker(latlng, { icon }).addTo(map.value);
+      updateEllipseColors(
+        currentMarker.value.getLatLng().lat,
+        currentMarker.value.getLatLng().lng
+      );
+      currentMarker.value
+        .bindPopup(
+          `
+        <div>
+          <p>New Marker</p>
+          <Button class="remove-pin-btn">Remove Pin</Button>
+        </div>
+      `
+        )
+        .on("popupopen", () => {
+          document
+            .querySelector(".remove-pin-btn")
+            .addEventListener("click", () => {
+              removeMarker(currentMarker.value._leaflet_id);
+            });
+        })
+        .openPopup();
+    }
+  } else {
+    alert("Click within an ellipse to add a marker.");
+  }
+};
+
+const removeMarker = (markerId) => {
+  if (currentMarker.value && currentMarker.value._leaflet_id === markerId) {
+    map.value.removeLayer(currentMarker.value);
+    updateEllipseColorsOnRemove(
+      currentMarker.value.getLatLng().lat,
+      currentMarker.value.getLatLng().lng
+    );
+    currentMarker.value = null;
+  }
+};
+
+const updateMarkerIconSize = (zoom) => {
+  const newIcon = updateIconSize(zoom);
+  if (currentMarker.value) {
+    currentMarker.value.setIcon(newIcon);
+  }
+};
+
+const updateIconSize = (zoom) => {
+  const baseSize = 20;
+  const newSize = baseSize * (zoom / 10);
+  const iconSize = [newSize, newSize * 1.64];
+
+  return L.value.icon({
+    iconUrl:
+      "https://img.icons8.com/?size=100&id=H48KNciLSEu5&format=png&color=000000",
+    iconSize: iconSize,
+    iconAnchor: [iconSize[0] / 2, iconSize[1]],
+    popupAnchor: [1, -iconSize[1] / 2],
+  });
+};
+
+const fetchAllMarkersAndUserBookings = async () => {
+  if (process.client && L.value) {
+
+    try {
+      const userBookings  = await $fetch("/api/check-user-booking", {
+        method: "GET",
+        params: {
+          startTime: startTime.value,
+          endTime: endTime.value,
+          userEmail: userEmail.value,
+        },
+      });
+
+      const markers  = await $fetch("/api/get-markers", {
+        method: "GET",
+        params: {
+          startTime: startTime.value,
+          endTime: endTime.value,
+          userEmail: userEmail.value,
+        },
+      });
+
+      clearMarkers();
+
+      // Process user bookings
+      // console.log(userBookings);
+      if (
+        userBookings &&
+        Array.isArray(userBookings)
+      ) {
+        console.log('Hi')
+        userBookings.forEach((booking) => {
+          let startDate = new Date(booking.startTime);
+          let endDate = new Date(booking.endTime);
+
+          startDate.setHours(startDate.getHours() - 4);
+          endDate.setHours(endDate.getHours() - 4);
+
+          const userMarker = L.value
+            .marker([booking.Latitude, booking.Longitude], {
+              icon: getUserBoatIcon(),
+            })
+            .bindPopup(
+              `
+          <b>Your Booking:</b><br>
+          Start: ${startDate.toLocaleString()}<br>
+          End: ${endDate.toLocaleString()}<br>
+          <button class="deleteBookingBtn" data-booking-id="${
+            booking.Id
+          }">Delete Booking</button>
+        `
+            )
+            .addTo(map.value);
+
+          currentMarker.value = userMarker;
+
+          userMarker.on("popupopen", () => {
+            const deleteButton = document.querySelector(".deleteBookingBtn");
+            deleteButton.addEventListener("click", async () => {
+              const bookingId = deleteButton.getAttribute("data-booking-id");
+              if (bookingId) {
+                await deleteUserBooking(bookingId, userMarker, userEmail.value);
+              }
+            });
+          });
+
+          userMarker.openPopup();
+          updateEllipseColors(booking.Latitude, booking.Longitude);
+        });
+      }
+
+      // Process other markers
+
+      console.log(markers); 
+      if (markers && Array.isArray(markers)) {
+        markers.forEach((marker) => {
+          let startDate = new Date(marker.startTime);
+          let endDate = new Date(marker.endTime);
+
+          startDate.setHours(startDate.getHours() - 4);
+          endDate.setHours(endDate.getHours() - 4);
+
+          L.value.marker([marker.Latitude, marker.Longitude], {
+            icon: getBoatIcon(),
+          })
+            .bindPopup(
+              `
+          <div>
+            <p>Start Time: ${startDate.toLocaleString()}</p>
+            <p>End Time: ${endDate.toLocaleString()}</p>
+          </div>
+        `
+            )
+            .addTo(map.value);
+
+          updateEllipseColors(marker.Latitude, marker.Longitude);
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+};
+
+const deleteUserBooking = async (bookingId, marker, userEmail) => {
+  const { data } = await useFetch("/api/delete-user-booking", {
+    method: "POST",
+    body: { bookingId, userEmail },
+  });
+
+  if (data.value.success) {
+    alert("Booking deleted successfully");
+    map.value.removeLayer(marker);
+    resetMap();
+    initializeMap();
+  } else {
+    alert("Error deleting booking: " + data.value.message);
+  }
+};
+
+const updateEllipseColors = (lat, lng) => {
+  ellipses.value.forEach((ellipse, index) => {
+    if (ellipse.getBounds().contains([lat, lng])) {
+      ellipseMarkersCount.value[index]++;
+      const count = ellipseMarkersCount.value[index];
+      let color = "blue";
+
+      if (count > 8) {
+        color = "red";
+      } else if (count > 5) {
+        color = "orange";
+      } else if (count > 3) {
+        color = "yellow";
+      }
+
+      ellipse.setStyle({
+        color,
+        fillColor: color,
+      });
+    }
+  });
+};
+
+const updateEllipseColorsOnRemove = (lat, lng) => {
+  ellipses.value.forEach((ellipse, index) => {
+    if (ellipse.getBounds().contains([lat, lng])) {
+      ellipseMarkersCount.value[index]--;
+      const count = ellipseMarkersCount.value[index];
+      let color = "blue";
+
+      if (count > 8) {
+        color = "red";
+      } else if (count > 5) {
+        color = "orange";
+      } else if (count > 3) {
+        color = "yellow";
+      }
+
+      ellipse.setStyle({
+        color,
+        fillColor: color,
+      });
+    }
+  });
+};
+
+const submitTimes = () => {
+  if (
+    startTime.value &&
+    endTime.value &&
+    new Date(startTime.value) < new Date(endTime.value)
+  ) {
+    if (mapInitialized.value) {
+      resetMap();
+      initializeMap();
+    } else {
+      initializeMap();
+    }
+
+    mapInitialized.value = true;
+    document.getElementById("mapContainer").style.display = "block";
+    document.getElementById("sendData").style.display = "inline";
+
+    fetchAllMarkersAndUserBookings();
+  } else {
+    alert("Please select valid start and end times.");
+  }
+};
+
+const clearMarkers = () => {
+  map.value.eachLayer((layer) => {
+    if (layer instanceof L.value.Marker) {
+      map.value.removeLayer(layer);
+    }
+  });
+};
+
+const getBoatIcon = () => {
+  if (!L.value) return null;
+  return L.value.icon({
+    iconUrl:
+      "https://img.icons8.com/?size=100&id=15133&format=png&color=000000",
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
+};
+
+const getUserBoatIcon = () => {
+  if (!L.value) return null;
+  return L.value.icon({
+    iconUrl:
+      "https://img.icons8.com/?size=100&id=UWVpDObu20sy&format=png&color=000000",
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
+};
+
+const resetMap = () => {
+  if (map.value) {
+    map.value.off();
+    map.value.remove();
+    map.value = null;
+  }
+  currentMarker.value = null;
+  ellipseMarkersCount.value = {};
+};
+
+const createEllipses = () => {
+  if (!L.value) return [];
+
+  const ellipseData = [
+    { center: [-19.940723, 57.593748], size: [10000, 5000], angle: -20 },
+    { center: [-20.042508, 57.479437], size: [7500, 5000], angle: -50 },
+    { center: [-20.152414, 57.39657], size: [8000, 5000], angle: -30 },
+    { center: [-20.269537, 57.320869], size: [8000, 5000], angle: -70 },
+    { center: [-20.397202, 57.31483], size: [7000, 4500], angle: -70 },
+    { center: [-20.508699, 57.324432], size: [7000, 4500], angle: 30 },
+    { center: [-20.540205, 57.450513], size: [6500, 4000], angle: 0 },
+  ];
+
+  return ellipseData.map((data, index) => {
+    ellipseMarkersCount.value[index] = 0;
+    return L.value
+      .ellipse(data.center, data.size, data.angle, {
+        color: "blue",
+        fillColor: "blue",
+        fillOpacity: 0.2,
+      })
+      .addTo(map.value);
+  });
+};
+
+const sendMarkerData = async () => {
+  if (currentMarker.value) {
+    const markerData = {
+      userEmail: userEmail.value,
+      latitude: currentMarker.value.getLatLng().lat,
+      longitude: currentMarker.value.getLatLng().lng,
+      startTime: new Date(startTime.value).toISOString(),
+      endTime: new Date(endTime.value).toISOString(),
+      markerId: currentMarker.value._leaflet_id,
+    };
+
+    const { data } = await useFetch("/api/send-boat-data", {
+      method: "POST",
+      body: markerData,
+    });
+
+    if (data.value.success) {
+      alert("Marker data sent successfully.");
+    } else {
+      alert("Error sending marker data: " + data.value.message);
+    }
+  } else {
+    alert("Please add a marker before sending data.");
+  }
+};
+</script>
+
+<style scoped>
+#map {
+  height: 1000px;
+  width: 100%;
+}
+#controls {
+  margin-bottom: 10px;
+}
+#mapContainer {
+  display: block;
+}
+
+/* @import 'leaflet/dist/leaflet.css'; */
+</style>
