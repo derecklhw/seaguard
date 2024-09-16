c64f9ba73e8d65a318a41d0508b971fd368d1d91
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2024-09-16 18:34:11 +0400

    feat: create the header component and add the differnt language for the pages

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,43 +1,102 @@
 <template>
-  <div class="bg-red-400">
-    <p>header start</p>
-    <div v-if="profile">
-      <p>{{ profile.displayName }}</p>
-      <div class="button" @click="logout">
-        <button class="logout-btn">Logout</button>
-      </div>
+  <header
+    class="sticky top-0 flex h-20 items-center gap-4 border-b bg-background px-4 md:px-6"
+  >
+    <nav
+      class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-8"
+    >
+      <img src="/images/logo.png" alt="Logo" class="h-12 w-12" />
+      <NuxtLink
+        :to="localePath('/')"
+        class="text-foreground transition-colors hover:text-foreground"
+      >
+        Home
+      </NuxtLink>
+
+      <NuxtLink
+        :to="localePath('e-learning')"
+        class="text-muted-foreground transition-colors hover:text-foreground"
+      >
+        {{ $t("e-learning") }}
+      </NuxtLink>
+      <NuxtLink
+        :to="localePath('map')"
+        class="text-muted-foreground transition-colors hover:text-foreground"
+      >
+        {{ $t("map") }}
+      </NuxtLink>
+      <a
+        href="#"
+        class="text-muted-foreground transition-colors hover:text-foreground"
+      >
+        Incidents
+      </a>
+      <a
+        href="#"
+        class="text-muted-foreground transition-colors hover:text-foreground"
+      >
+        Donation
+      </a>
+      <a
+        href="#"
+        class="text-muted-foreground transition-colors hover:text-foreground"
+      >
+        Quiz
+      </a>
+    </nav>
+    <Sheet>
+      <SheetTrigger as-child>
+        <Button variant="outline" size="icon" class="shrink-0 md:hidden">
+          <IconMenu class="h-5 w-5" />
+          <span class="sr-only">Toggle navigation menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <nav class="grid gap-6 text-lg font-medium">
+          <img src="/images/logo.png" alt="Logo" class="h-12 w-12" />
+          <span class="sr-only">Acme Inc</span>
+
+          <a href="#" class="hover:text-foreground"> Home </a>
+          <a href="#" class="text-muted-foreground hover:text-foreground">
+            E-Learning
+          </a>
+          <a href="#" class="text-muted-foreground hover:text-foreground">
+            Map
+          </a>
+          <a href="#" class="text-muted-foreground hover:text-foreground">
+            Incidents
+          </a>
+          <a href="#" class="text-muted-foreground hover:text-foreground">
+            Donation
+          </a>
+          <a href="#" class="text-muted-foreground hover:text-foreground">
+            Quiz
+          </a>
+        </nav>
+      </SheetContent>
+    </Sheet>
+    <div
+      class="flex w-80 items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4"
+    >
+      <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+          <Button variant="secondary" size="icon" class="rounded-full">
+            <IconCircleUser class="h-5 w-5" />
+            <span class="sr-only">Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
-    <div v-else class="button">
-      <button class="btn" @click="loginUser">
-        Click to login with Microsoft 365
-      </button>
-    </div>
-    <p>header end</p>
-  </div>
+  </header>
 </template>
 <script setup>
-const { $login, $profileInfo, $logout } = useNuxtApp();
-const profile = ref();
-
-onMounted(async () => {
-  const profileInfo = await $profileInfo();
-  profile.value = profileInfo;
-});
-
-const loginUser = async () => {
-  clearSiteData();
-  const loginResponse = await $login();
-  if (loginResponse) reloadNuxtApp();
-};
-const logout = async () => {
-  await $logout();
-};
-function clearSiteData() {
-  document.cookie.split(";").forEach((cookie) => {
-    const [name, _] = cookie.split("=").map((c) => c.trim());
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-  });
-  localStorage.clear();
-  sessionStorage.clear();
-}
+const localePath = useLocalePath();
 </script>
---
+++ pages/map.vue
@@ -0,0 +1,1 @@
+<template><p>map</p></template>
