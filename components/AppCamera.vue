96944cfd34e6f4ef19452b33cf1db73e8e82cb70
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2024-09-19 21:40:38 +0400

    feat: move camera to component and create a register page

---
+++ components/AppCamera.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <ClientOnly>
+      <WebCamUI
+        v-if="cameraActive"
+        :fullscreen-state="true"
+        @photo-taken="photoTaken"
+        @fullscreen="handleFullscreenEvent"
+      />
+    </ClientOnly>
+    <Button @click="toggleCamera">Take Photo</Button>
+  </div>
+</template>
+
+<script setup>
+import { WebCamUI } from "vue-camera-lib";
+
+const cameraActive = ref(false);
+
+const props = defineProps(["type"]);
+const emit = defineEmits(["photoAnalysis"]);
+
+const toggleCamera = () => {
+  cameraActive.value = !cameraActive.value;
+};
+
+const photoTaken = (data) => {
+  emit("photoAnalysis", "loading");
+  const model =
+    props.type === "license" ? "MruBoatLicenseModel" : "prebuilt-layout";
+
+  const fd = new FormData();
+  if (data.blob && data.image_data_url) {
+    fd.append("index", data.image_data_url);
+    fd.append("isPhotoTaken", "true");
+    fd.append("model", model);
+  }
+
+  $fetch("/api/extract-text-from-document", {
+    method: "POST",
+    body: fd,
+  }).then((response) => {
+    emit("photoAnalysis", response);
+  });
+};
+
+const handleFullscreenEvent = (currentState) => {
+  if (!currentState) {
+    cameraActive.value = false;
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ pages/register.vue
@@ -0,0 +1,8 @@
+<template>
+  <AppCamera :type="'license'" @photo-analysis="photoAnalysis" />
+</template>
+<script setup>
+const photoAnalysis = (data) => {
+  console.log("photo taken", data);
+};
+</script>
